046c258aaed9acd6d3311918de749354548c5118
MarissaShieh <marissa.shieh@gmail.com>
2019-03-08 19:58:53 -0800

    Move swimmers in client based on randomly generated server directions

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,6 +1,16 @@
-(function() {
+
+
+setInterval((function() {
 
   const serverUrl = 'http://127.0.0.1:3000';
-  $.ajax
 
-})();
+  $.get({
+    url: serverUrl,
+    type: 'GET',
+    success: (direction) => {
+       SwimTeam.move(direction);
+    },
+    error: () => console.log("There has been an error!")
+  })
+
+}),200);
